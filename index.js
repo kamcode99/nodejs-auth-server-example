26530c485f2de70bc4ee56c14cfb78eafbd97877
Kamal K Garg <kamalkgarg@gmail.com>
2016-05-27 05:39:25 +0530

    Added bodyparser and morgan middlewares

--- index.js
+++ index.js
@@ -2,13 +2,26 @@
 // http is the native node library
 const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
+// BodyParser is a middleware which is used
+// to parse the incomming request specifically
+// it parse the request into json.
+const bodyParser = require('body-parser');
+// Morgan is a logging framework and a middleware
+// Which is basically used for debugging.
+const morgan = require('morgan');
 const app = express();
 
 //**** Application Setup ****
-
+// Both morgan and bodyParser are middleware in express
+// So any incomming request will be passed into morgan
+// and also to bodyParser.'app.use' is used to register
+// morgan and bodyParser as middleware.
+app.use(morgan('combined'));
+irrespective
+// 'type: */*' says that parse every incomming request
+// into json irrespectiveof the request type.
+app.use(bodyParser.json({type: '*/*'}));
 
 
 //**** Server Setup ****
